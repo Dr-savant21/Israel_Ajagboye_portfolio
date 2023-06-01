30ce9fda39ecf78d2a8498ec7f0188fd8afd9815
Dr-savant21 <israelnosakhare21@gmail.com>
2023-06-01 22:18:26 +0100

    some more features added like animation and changes made to some icons

--- js/script.js
+++ js/script.js
@@ -1,82 +1,94 @@
-// const navBar = document.querySelector('#navScrollSpy');
-// const sections = document.querySelector('.sect')
-// if (document.body.offsetHeight == '100px'){
-//     navBar.classList.toggle('sticky')
-// }
-// navBar.addEventListener('scroll',scrollevent)
-// function scrollevent(){
-//     navBar.classList.toggle('sticky')
-// }
+(function () {
+    "use strict";
 
-// function nameLoop(){
-//     let intro = ['I',' ', 'a','m',' ','I','s','r','a','e','l',' ','A','j','a','b','g','b','o','y','e']
-//     let newintro = ''
-//     for (let i=0; i<intro.length; i++){
-//         newintro += intro[i];
-//         console.log(newintro)
-//     }
-//     const myName = document.getElementById('name');
-//     myName.innerHTML = newintro
-//     myName.style.color = 'red';
-// }
-// setTimeout(nameLoop,5000);
-// nameLoop();
-// let intro = ['I',' ', 'a','m',' ','I','s','r','a','e','l',' ','A','j','a','b','g','b','o','y','e']
-// function designerName(){
-//     for (let i=0; i<intro.length; i++) {
-//         let newintro = ''
-//         newintro = intro[i];
-//         // console.log(newintro)
-//         const myName = document.getElementById('name');
-//         myName.innerHTML = newintro
-//         myName.style.color = 'red';
-//     }
-// }
-// setInterval(designerName,1000)
+    /* Easy selector helper function */
+    const select = (el, all = false) => {
+        el = el.trim()
+        if (all) {
+            return [...document.querySelectorAll(el)]
+        } else {
+            return document.querySelector(el)
+        }
+    }
 
-// var words = ['Hi i like HTML', 'I also like css', 'Lorem ipsum dolor sit amet', ' consectetur adipiscing elit', 'sed do eiusmod tempor incididunt'],
-//     part,
-//     i = 0,
-//     offset = 0,
-//     len = words.length,
-//     forwards = true,
-//     skip_count = 0,
-//     skip_delay = 15,
-//     speed = 70;
-// var wordflick = function () {
-//   setInterval(function () {
-//     if (forwards) {
-//       if (offset >= words[i].length) {
-//         ++skip_count;
-//         if (skip_count == skip_delay) {
-//           forwards = false;
-//           skip_count = 0;
-//         }
-//       }
-//     }
-//     else {
-//       if (offset == 0) {
-//         forwards = true;
-//         i++;
-//         offset = 0;
-//         if (i >= len) {
-//           i = 0;
-//         }
-//       }
-//     }
-//     part = words[i].substr(0, offset);
-//     if (skip_count == 0) {
-//       if (forwards) {
-//         offset++;
-//       }
-//       else {
-//         offset--;
-//       }
-//     }
-//     $('.word').text(part);
-//   },speed);
-// };
+    /* Easy event listener function */
+    const on = (type, el, listener, all = false) => {
+        let selectEl = select(el, all)
+        if (selectEl) {
+            if (all) {
+                selectEl.forEach(e => e.addEventListener(type, listener))
+            } else {
+                selectEl.addEventListener(type, listener)
+            }
+        }
+    }
 
-// $(document).ready(function () {
-//   wordflick();
-// });
+    /* Easy on scroll event listener  */
+    const onscroll = (el, listener) => {
+        el.addEventListener('scroll', listener)
+    }
+
+    /* Toggle .header-scrolled class to #header when page is scrolled */
+    let selectHeader = select('#header')
+    if (selectHeader) {
+        const headerScrolled = () => {
+            if (window.scrollY > 100) {
+                selectHeader.classList.add('header-scrolled')
+            } else {
+                selectHeader.classList.remove('header-scrolled')
+            }
+        }
+        window.addEventListener('load', headerScrolled)
+        onscroll(document, headerScrolled)
+    }
+
+    /* Back to top button */
+    let backtotop = select('.arrow-up')
+    if (backtotop) {
+        const toggleBacktotop = () => {
+            if (window.scrollY > 100) {
+                backtotop.classList.add('active')
+            } else {
+                backtotop.classList.remove('active')
+            }
+        }
+        window.addEventListener('load', toggleBacktotop)
+        onscroll(document, toggleBacktotop)
+    }
+
+    /* Preloader*/
+    let preloader = select('#preloader');
+    if (preloader) {
+        window.addEventListener('load', () => {
+            preloader.remove()
+        });
+    }
+
+    /* Skills animation */
+    let skilsContent = select('.skills-content');
+    if (skilsContent) {
+        new Waypoint({
+            element: skilsContent,
+            offset: '80%',
+            handler: function (direction) {
+                let progress = select('.progress .progress-bar', true);
+                progress.forEach((el) => {
+                    el.style.width = el.getAttribute('aria-valuenow') + '%'
+                });
+            }
+        })
+    }
+
+    /* Animation on scroll */
+    window.addEventListener('load', () => {
+        AOS.init({
+            duration: 1000,
+            easing: 'ease-in-out',
+            once: true,
+            mirror: false
+        })
+    });
+
+    /* Initiate Pure Counter */
+    new PureCounter();
+})()
